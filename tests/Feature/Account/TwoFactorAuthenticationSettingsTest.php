c3179b0fd721f7b90dc3b83a65d52ff47a9f8b26
secondman <secondman@jetstreamlabs.com>
2023-03-13 20:10:53 -0700

    fix: failing two factor test
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- tests/Feature/Account/TwoFactorAuthenticationSettingsTest.php
+++ tests/Feature/Account/TwoFactorAuthenticationSettingsTest.php
@@ -21,11 +21,11 @@ it('can generate recovery codes', function () {
   $this->withSession(['auth.password_confirmed_at' => time()]);
 
   $this->post(route('two-factor.enable'));
-  $this->post('/user/two-factor/recovery/codes');
+  $this->post('/user/two-factor-recovery-codes');
 
   $user = $user->fresh();
 
-  $this->post('/user/two-factor/recovery/codes');
+  $this->post('/user/two-factor-recovery-codes');
 
   expect($user->recoveryCodes())->toHaveCount(8);
   expect(array_diff($user->recoveryCodes(), $user->fresh()->recoveryCodes()))->toHaveCount(8);
